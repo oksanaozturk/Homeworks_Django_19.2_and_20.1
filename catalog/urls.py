ec04b9472e436dde2e64949df343b0023510e206
casper <oksanaozturk@gmail.com>
2024-02-27 15:41:23 +0300

    Настройка для media  перенесена из config/urls в catalog/ urls: + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from catalog.views import home_page, contacts
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', home_page, name='home_page'),
     path('contacts/', contacts, name='contacts')
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Другой вариант прописывания media путей
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
